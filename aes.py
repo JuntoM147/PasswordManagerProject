c31ba935d37af80fe70196de7eca8beb496f541d
Junto Mabuchi <juntomabuchi@hotmail.com>
2024-10-30 16:44:08 +0000

    Started aes class and functions

---
+++ aes.py
@@ -0,0 +1,22 @@
+class aes:
+    def __init__(self):
+        self.key_size = 128
+        self.total_rounds = 10
+        self.curr_round = 0
+    
+    def keyExpansion(self):
+        pass
+
+    def addRoundKey(self):
+        pass
+    
+    def subBytes(self):
+        pass
+
+    def shiftRows(self):
+        pass
+
+    def mixColumns(self):
+        pass
+
+    
